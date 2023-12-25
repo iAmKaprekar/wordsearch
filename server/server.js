7bbc52d4ca65d37913b49f450b6a3d033a7ba0c3
Nathan Gonsalves <iamkaprekar@gmail.com>
2023-12-25 12:40:50 -0500

    Functional

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, '../client/assets')));
+
+app.use('/', express.static(path.resolve(__dirname, '../build')));
+app.use('/assets', express.static(path.resolve(__dirname, '../assets')));
+
+app.get('/', (req, res) => {
+  return res.status(200).sendFile('../client/index.html');
+})
+
+app.listen(PORT, () => {
+  console.log(`Wordsearch server listening on port ${PORT}...`)
+});
